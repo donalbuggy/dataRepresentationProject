4ac9701fe4f2e1af3390892da1b064b5bbdaa659
donalbuggy <buggyjdonal@gmail.com>
2022-12-21 18:54:10 +0000

    adding initial files

---
+++ server.py
@@ -0,0 +1,9 @@
+from flask import Flask, jsonify, request, abort
+
+app = Flask(__name__, static_url_path='', static_folder='.')
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
